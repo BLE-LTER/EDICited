b19ccf4afbbaebf4b6134f6a5ff16a753441d221
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2023-06-01 17:18:30 -0500

    clean_identifier to also remove whitespace

--- R/utils.R
+++ R/utils.R
@@ -9,11 +9,12 @@
 #'
 #' @examples
 clean_identifier <- function(identifier) {
-  if (!all(grepl("https://doi.org", identifier))) {
-    identifier <- sub("https://doi.org", "", identifier)
+  if (any(grepl("https://doi.org", identifier))) {
+    identifier <- sub("https://doi.org/", "", identifier)
   }
   if (any(grepl("doi:", identifier))) {
     identifier <- sub("doi:", "", identifier)
   }
+  identifier <- trimws(sub(" ", "", identifier))
   return(identifier)
 }
