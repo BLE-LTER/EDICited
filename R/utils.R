1432464044ab380c39ee42f774d9539aa37c23f7
anthainguyen38 <an.thai.nguyen.38@gmail.com>
2023-06-07 18:46:57 -0500

    trim whitespace in DOIs

--- R/utils.R
+++ R/utils.R
@@ -9,11 +9,14 @@
 #'
 #' @examples
 clean_identifier <- function(identifier) {
-  if (!all(grepl("https://doi.org", identifier))) {
-    identifier <- sub("https://doi.org", "", identifier)
+  identifier <- trimws(sub(" ", "", identifier))
+
+  if (any(grepl("https://doi.org", identifier))) {
+    identifier <- sub("https://doi.org/", "", identifier)
   }
   if (any(grepl("doi:", identifier))) {
     identifier <- sub("doi:", "", identifier)
   }
+  identifier <- trimws(sub(" ", "", identifier))
   return(identifier)
 }
